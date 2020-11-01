ef219542cc0edca61442327f83cde59ffe7a7b45
ekater1na <ekaterina_l@tut.by>
2020-11-01 17:32:41 +0300

    build: clean main branch

--- shop-info-service/handler.ts
+++
@@ -1,6 +0,0 @@
-import 'source-map-support/register';
-
-const getShopAdminInfo = require('./handlers/getShopAdminInfo');
-const getShopInfo = require('./handlers/getShopInfo');
-
-export {getShopAdminInfo, getShopInfo};
